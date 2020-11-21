d84c7b158f7621a16c359eec299de3e9fbe22817
kik0908 <kik0908@yandex.ru>
2020-11-21 11:43:17 +0300

    1

--- HomeWork/myPhotoshop/myPhotoshop.cpp
+++ HomeWork/myPhotoshop/myPhotoshop.cpp
@@ -5,24 +5,32 @@
 
 int main()
 {	
-	char filename[] = "test2.bmp";
+	char filename[] = "test3.bmp";
 
 	RgbImg img = read_rgb_img(filename);
-	create_noice(img, 400);
+	create_noice(img, 1500);
 	write_rgb_img("out.bmp", img);
+
+	std::cout << "Noice" << '\n';
 
 	RgbImg new_img = to_black_white_img(img);
 	write_rgb_img("out1.bmp", new_img);
 	delete[] new_img.pixels;
 
-	
+	std::cout << "Black" << '\n';
 
 	new_img = blur_filter(img, (size_t)1);
 	write_rgb_img("out2.bmp", new_img);
 	delete[] new_img.pixels;
 
+	std::cout << "Blur" << '\n';
+
 	new_img = median_filter(img, (size_t)1);
 	write_rgb_img("out3.bmp", new_img);
+	new_img = median_filter(img, (size_t)2);
+	write_rgb_img("out4.bmp", new_img);
+	new_img = median_filter(img, (size_t)5);
+	write_rgb_img("out5.bmp", new_img);
 
 
 	return 0;
